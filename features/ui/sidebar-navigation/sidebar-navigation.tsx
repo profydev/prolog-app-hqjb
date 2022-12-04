673db3d920401a659f839bb2ee66ccde9317fb79
Hqjb <hqjb@hotmail.sg>
2022-12-04 09:35:47 +0800

    Fix navigation support button (#4)
    * Fixed sidebar navigation arrow to point to right when collapsed
    
    * Fixed separator in project card
    
    * Fixed Issue List's Pagination Color
    
    * Fixed support button should open mail app
    
    * Changed Menu Item Button to Link
    
    Co-authored-by: He Quanjie Boey <hqjb91@gmail.com>

--- features/ui/sidebar-navigation/sidebar-navigation.tsx
+++ features/ui/sidebar-navigation/sidebar-navigation.tsx
@@ -195,11 +195,12 @@ export function SidebarNavigation() {
           </LinkList>
 
           <List>
-            <MenuItemButton
+            <MenuItemLink
               text="Support"
               iconSrc="/icons/support.svg"
               isCollapsed={isSidebarCollapsed}
-              onClick={() => alert("Support")}
+              isActive
+              href="mailto:support@prolog-app.com?subject=Support Request:"
             />
             <CollapseMenuItem
               text="Collapse"
